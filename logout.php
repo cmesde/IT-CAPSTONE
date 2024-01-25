96df4d6835e1a91ba52b8f9455aeeda0e4d6cf89
cmesde <83897431+cmesde@users.noreply.github.com>
2024-01-25 13:57:32 +0800

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,7 +1,9 @@
-<?php  
+<?php
 session_start();
-
-session_unset();
-session_destroy();
-
-header("Location: login-index.php");
+if(session_destroy())
+{
+	include 'db.php';
+	mysqli_query($conn,"INSERT INTO history_log (transaction,user_id,date_added)VALUES('logged out','".$_SESSION['ID']."',NOW()) ");
+header("Location: login.php");
+}
+?>
